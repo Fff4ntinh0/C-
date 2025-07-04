2693de14d61992b54bbce0817f31566d53ed3e33
Fff4ntinh0 <kauamouradosanjos@gmail.com>
2025-07-04 08:19:23 -0300

    finalizando o primeiro exerciscio

---
+++ exercises/01a/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("My programming languages: ");
+Console.WriteLine(" - C++");
+Console.WriteLine(" - C#");
+Console.WriteLine(" - Python");
+Console.WriteLine(" - Java");
+Console.WriteLine(" - Javascript");
