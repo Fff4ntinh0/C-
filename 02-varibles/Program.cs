103bbdf7f44ab22e42bf99b9927cffe86629dd8b
Fff4ntinh0 <kauamouradosanjos@gmail.com>
2025-07-05 08:29:07 -0300

    2

---
+++ 02-varibles/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Metadata;
+
+int n1 = 3;
+string s1 = "Hello World ";
+float f1 = 1.234F;
+double d1 = -1.25849020393484;
+char c1 = '!';
+bool b1 = true;
+
+Console.WriteLine(s1 + c1);
